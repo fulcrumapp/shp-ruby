ffc7d5ba5b4d1081b37457ee4fc0b6f2529fd690
Zac McCormick <zac.mccormick@gmail.com>
2013-10-10 19:35:43 -0400

    Export the constants

--- lib/shp.rb
+++ lib/shp.rb
@@ -1,2 +1,19 @@
 require 'shp/version'
 require 'shp/shp'
+
+module SHP
+  SHPT_NULL        = 0
+  SHPT_POINT       = 1
+  SHPT_ARC         = 3
+  SHPT_POLYGON     = 5
+  SHPT_MULTIPOINT  = 8
+  SHPT_POINTZ      = 11
+  SHPT_ARCZ        = 13
+  SHPT_POLYGONZ    = 15
+  SHPT_MULTIPOINTZ = 18
+  SHPT_POINTM      = 21
+  SHPT_ARCM        = 23
+  SHPT_POLYGONM    = 25
+  SHPT_MULTIPOINTM = 28
+  SHPT_MULTIPATCH  = 31
+end
