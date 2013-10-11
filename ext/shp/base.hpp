f0d74b6e9ef9426d8baee61ffc32de452fbae703
Zac McCormick <zac.mccormick@gmail.com>
2013-10-11 09:46:17 -0400

    Add support for mark phase of the ruby GC

--- ext/shp/base.hpp
+++ ext/shp/base.hpp
@@ -1,12 +1,13 @@
 #ifndef __SHP_BASE_HPP__
 #define __SHP_BASE_HPP__
 
+#include <vector>
 #include "shp.hpp"
 
 namespace shp {
   template <typename T> class base {
   public:
-    base<T>() : _value(0) {}
+    base<T>() : _value(0), dependencies() {}
 
     static VALUE initialize_instance(int argc, VALUE *argv, VALUE self) {
       return unwrap(self)->initialize(argc, argv);
@@ -31,18 +32,23 @@ namespace shp {
 
     VALUE wrapped() {
       if (!_value) {
-        _value = Data_Wrap_Struct(klass(), base<T>::mark, base<T>::dispose, this);
+        _value = Data_Wrap_Struct(klass(), base<T>::mark_dependencies, base<T>::dispose, this);
       }
       return _value;
     }
 
-    void mark_self() {
+    void gc_mark() {
       if (_value) {
         rb_gc_mark(_value);
       }
     }
 
-    virtual void mark() {}
+    virtual void mark_dependencies() {
+      for (typename std::vector<base *>::iterator it = dependencies.begin(); it != dependencies.end(); ++it) {
+        (*it)->gc_mark();
+      }
+    }
+
     virtual void dispose() {}
     virtual VALUE initialize(int argc, VALUE *argv) { return wrapped(); }
 
@@ -58,15 +64,25 @@ namespace shp {
       return obj;
     }
 
+    void add_dependency(base *dep) {
+      dependencies.push(dep);
+    }
+
+    void remove_dependency(base *dep) {
+      dependencies.erase(std::remove(dependencies.begin(), dependencies.end(), dep), dependencies.end());
+    }
+
   protected:
     VALUE _value;
+
+    std::vector<base *> dependencies;
 
     static T *object(void *ptr) {
       return static_cast<T *>(ptr);
     }
 
-    static void mark(void *ptr) {
-      base::object(ptr)->mark();
+    static void mark_dependencies(void *ptr) {
+      base::object(ptr)->mark_dependencies();
     }
 
     static void dispose(void *ptr) {
